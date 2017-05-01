b3a18e499efb4cd019f55edd931438b8f18af968
Mark Winterbottom <mark@londonappdeveloper.com>
2017-05-01 17:53:16 +0100

    Added flex dimensions.

--- index.android.js
+++ index.android.js
@@ -107,5 +107,17 @@ class FixedDimensionsBasics extends Component {
   }
 }
 
+class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+}
 
-AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
+
+AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);
